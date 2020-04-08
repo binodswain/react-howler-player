cc9c8c10c6e7e2d032dd01892eaebe60a7096133
Binod Swain <binodkumarswain94@gmail.com>
2020-04-08 23:39:07 +0530

    feat(): added loading component prop
    added preparingComp prop in Player comp
    added Loading component in example folder

--- example/src/App.js
+++ example/src/App.js
@@ -1,110 +1,122 @@
-import React, { Component, PureComponent } from 'react';
+import React, { PureComponent } from "react";
 import Player from "react-howler-player";
 
+function Loading() {
+    return (
+        <div>
+            <h4>text from loading component</h4>
+        </div>
+    );
+}
+
 export default class App extends PureComponent {
-    state = {}
+    state = {};
     onChange = (event) => {
         if (!event.target.files[0]) {
-            return
+            return;
         }
-        
+
         let file_path = URL.createObjectURL(event.target.files[0]);
 
-        const {
-            type,
-            name
-        } = event.target.files[0];
+        const { type, name } = event.target.files[0];
 
-        this.setState((prevState, props) => ({
+        this.setState(() => ({
             file_path,
             type,
-            name
+            name,
         }));
-    }
+    };
 
     timeUpdate = (attrs) => {
         // console.log(attrs);
-    }
+    };
 
     removeAudio = (e) => {
         e.preventDefault();
         this.setState({
             file_path: null,
             type: null,
-            name: null
-        })
-    }
+            name: null,
+        });
+    };
 
     change = (e) => {
-        console.log(e.target);
-        const {
-            name, value
-        } = e.target;
-        this.setState({[name]: value})
-    }
+        const { name, value } = e.target;
+        this.setState({ [name]: value });
+    };
 
     render() {
         const { file_path, name } = this.state;
         return (
             <div>
-                <h1>react-howler-player<br/>example</h1>
+                <h1>
+                    react-howler-player
+                    <br />
+                    example
+                </h1>
                 <div className="playerlist">
                     <h3>1. Audio from remote url</h3>
-                    <h5><a href="http://https://librivox.org/pride-and-prejudice-by-jane-austen-solo-project/" target="_blank" rel="noopener noreferrer"> Pride and Prejudice (version 2)</a></h5>
+                    <h5>
+                        <a
+                            href="http://https://librivox.org/pride-and-prejudice-by-jane-austen-solo-project/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            {" "}
+                            Pride and Prejudice (version 2)
+                        </a>
+                    </h5>
                     <Player
                         src={[
-                            'http://www.archive.org/download/solo_pride_librivox/prideandprejudice_01-04_austen_apc_64kb.mp3'
-                        ]}   
+                            "http://www.archive.org/download/solo_pride_librivox/prideandprejudice_01-04_austen_apc_64kb.mp3",
+                        ]}
                         onTimeUpdate={this.timeUpdate}
+                        preparingComp={<Loading />}
                     />
-                    <Player
+                    {/* <Player
                         src={[
-                            'http://www.archive.org/download/solo_pride_librivox/prideandprejudice_01-04_austen_apc_64kb.mp3'
-                        ]}   
+                            "http://www.archive.org/download/solo_pride_librivox/prideandprejudice_01-04_austen_apc_64kb.mp3",
+                        ]}
                         onTimeUpdate={this.timeUpdate}
                         isDark={true}
-                    />
-                    <p>Source: http://www.archive.org/download/solo_pride_librivox/prideandprejudice_01-04_austen_apc_64kb.mp3</p>
+                    /> */}
+                    <p>
+                        Source:
+                        http://www.archive.org/download/solo_pride_librivox/prideandprejudice_01-04_austen_apc_64kb.mp3
+                    </p>
                 </div>
-                
-                
+
                 <div>
                     <h3>2. Audio from local file</h3>
                     <div className="form-element">
-                        <input type="file" 
-                            className="file-input" 
-                            id="file-input" 
-                            name="audio_file" 
-                            accept="audio/*" 
+                        <input
+                            type="file"
+                            className="file-input"
+                            id="file-input"
+                            name="audio_file"
+                            accept="audio/*"
                             onChange={(e) => this.onChange(e)}
-                            id="audio-file"
                             hidden
                         />
                         <label htmlFor="audio-file">
-                            <div className="form-label">
-                                {name || 'Select an audio file'} 
-                            </div>
+                            <div className="form-label">{name || "Select an audio file"}</div>
 
-                            {file_path ? 
+                            {file_path ? (
                                 <div className="icon-close" onClick={this.removeAudio}>
-                                    <i className="material-icons">
-                                        close
-                                    </i>
+                                    <i className="material-icons">close</i>
                                 </div>
-                                :null
-                            }
+                            ) : null}
                         </label>
                     </div>
-                    {file_path ? 
+                    {file_path ? (
                         <Player
                             src={[file_path]}
-                            format={['wav', 'mp3']}
+                            format={["wav", "mp3"]}
                             onTimeUpdate={this.timeUpdate}
-                        /> : null
-                    }
+                        />
+                    ) : null}
                 </div>
-                
             </div>
-        )
+        );
     }
 }
--- src/prepare.js
+++ src/prepare.js
@@ -5,26 +5,23 @@ import style from "./styles.scss";
 export default class Prepare extends Component {
     static propTypes = {
         loadingText: PropTypes.string,
-        isDark: PropTypes.bool
+        isDark: PropTypes.bool,
+        preparingComp: PropTypes.any,
     };
 
     render() {
-        const {
-            loadingText = "Preparing",
-            isDark
-        } = this.props;
+        const { loadingText = "Preparing", isDark, preparingComp } = this.props;
 
         let className = [
-            style["player"], style["r-howler"],
-            style[isDark ? "dark-themed" : "light-themed"]
+            style["player"],
+            style["r-howler"],
+            style[isDark ? "dark-themed" : "light-themed"],
         ].join(" ");
 
-        return (
-            <div className={className}>
-                <div className={[style["prepare"],style["notready"]].join(" ")}>
-                    {loadingText}
-                </div>
-            </div>
+        const preparing = preparingComp || (
+            <div className={[style["prepare"], style["notready"]].join(" ")}>{loadingText}</div>
         );
+
+        return <div className={className}>{preparing}</div>;
     }
 }
