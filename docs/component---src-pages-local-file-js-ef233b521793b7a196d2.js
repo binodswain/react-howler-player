5312349af79c87a11f703542a56500c44c9ec127
Binod Swain <binodkumarswain94@gmail.com>
2020-06-06 21:03:33 +0530

    updated site, v@1.0.9

---
+++ docs/component---src-pages-local-file-js-ef233b521793b7a196d2.js
@@ -0,0 +1,126 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([
+    [4],
+    {
+        PhLX: function (e, n, t) {
+            
+            t.r(n);
+            t("f3/d");
+            const a = t("q1tI"),
+                l = t.n(a),
+                i = t("Bl7J"),
+                c = t("vrFN"),
+                o = t("jHpe"),
+                s = t.n(o),
+                r =
+                    (t("y1X9"),
+                    t("bPOv"),
+                    t("QWvX"),
+                    t("ZgVT"),
+                    t("85O/"),
+                    t("CjHp"),
+                    t("hL/g"),
+                    t("tHFp")),
+                m =
+                    (t("7kCs"),
+                    'import React, { useEffect, useState } from "react"\nimport Player from "react-howler-player";\n\n\nconst Component = () => {\n  const [filepath, setFilepath] = useState(\'\')\n  const [name, setName] = useState(\'\')\n\n  const onChange = (event) => {\n    if (!event.target.files[0]) {\n      return\n    }\n    let filepath = URL.createObjectURL(event.target.files[0]);\n    const {\n      type,\n      name\n    } = event.target.files[0];\n    setFilepath(filepath)\n    setName(name)\n  }\n  const removeAudio = (e) => {\n    e.preventDefault();\n    setFilepath(\'\')\n    setName(\'\')\n  }\n  return (\n    <div>\n      <div className="form-element">\n        <input type="file" \n            className="file-input" \n            id="file-input" \n            name="audio_file" \n            accept="audio/*" \n            onChange={(e) => onChange(e)}\n            id="audio-file"\n            hidden\n        />\n        <label htmlFor="audio-file">\n            <div className="form-label">\n                {name || \'Select an audio file\'} \n            </div>\n\n            {filepath ? \n                <div className="icon-close" onClick={(e)=>removeAudio(e)}>\n                    <i className="material-icons">\n                        close\n                    </i>\n                </div>\n                :null\n            }\n        </label>\n      </div>\n      {filepath ? \n          <Player\n              src={[filepath]}\n              format={[\'wav\', \'mp3\']}\n          /> : null\n      }\n    </div>\n  )\n}');
+            n.default = function () {
+                let e;
+                Object(a.useEffect)(function () {
+                    s.a.highlightAll(), t(!0);
+                });
+                var n = Object(a.useState)(!1),
+                    t = (n[0], n[1]),
+                    o = Object(a.useState)(""),
+                    u = o[0],
+                    f = o[1],
+                    d = Object(a.useState)(""),
+                    p = d[0],
+                    v = d[1],
+                    h = Object(a.useState)(!1),
+                    b = h[0],
+                    N = h[1];
+                return l.a.createElement(
+                    i.a,
+                    null,
+                    l.a.createElement(c.a, { title: "Local audio" }),
+                    l.a.createElement("h2", null, "Local audio file"),
+                    l.a.createElement(
+                        "div",
+                        { className: "form-element" },
+                        l.a.createElement(
+                            "input",
+                            (((e = {
+                                type: "file",
+                                className: "file-input",
+                                id: "file-input",
+                                name: "audio_file",
+                                accept: "audio/*",
+                                onChange: function (e) {
+                                    return (function (e) {
+                                        if (e.target.files[0]) {
+                                            const n = URL.createObjectURL(e.target.files[0]),
+                                                t = e.target.files[0],
+                                                a = (t.type, t.name);
+                                            f(n), v(a);
+                                        }
+                                    })(e);
+                                },
+                            }).id = "audio-file"),
+                            (e.hidden = !0),
+                            e),
+                        ),
+                        l.a.createElement(
+                            "label",
+                            { htmlFor: "audio-file" },
+                            l.a.createElement(
+                                "div",
+                                { className: "form-label" },
+                                p || "Select an audio file",
+                            ),
+                            u
+                                ? l.a.createElement(
+                                    "div",
+                                    {
+                                        className: "icon-close",
+                                        onClick: function (e) {
+                                            return (function (e) {
+                                                e.preventDefault(), f(""), v("");
+                                            })(e);
+                                        },
+                                    },
+                                    l.a.createElement(
+                                        "i",
+                                        { className: "material-icons" },
+                                        "close",
+                                    ),
+                                )
+                                : null,
+                        ),
+                    ),
+                    u ? l.a.createElement(r.a, { src: [u], format: ["wav", "mp3"] }) : null,
+                    l.a.createElement(
+                        "button",
+                        {
+                            onClick: function () {
+                                return N(!b);
+                            },
+                            className: "button-code",
+                        },
+                        b ? "Hide" : "Show",
+                        " Source code",
+                    ),
+                    l.a.createElement(
+                        "div",
+                        { className: ["expandable-section", b ? "expand" : "close"].join(" ") },
+                        l.a.createElement(
+                            "pre",
+                            { className: "line-numbers" },
+                            l.a.createElement("code", { className: "language-jsx" }, m),
+                        ),
+                    ),
+                );
+            };
+        },
+    },
+]);
+//# sourceMappingURL=component---src-pages-local-file-js-ef233b521793b7a196d2.js.map
--- docs/styles-bc7f2a998b197a593f49.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{"7kCs":function(n,o,c){},"8ypT":function(n,o,c){},RD2v:function(n,o,c){},"hL/g":function(n,o,c){}}]);
-//# sourceMappingURL=styles-bc7f2a998b197a593f49.js.map
